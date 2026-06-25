7c4052c38a136c64eec2c640940e9c646325c90f
Andrej Orlov <orlov.andrej@proton.me>
2026-06-25 14:47:45 +0200

    CH5.3

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
-
+import _ "github.com/lib/pq"
 import (
 	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
+	"github.com/joho/godotenv"
+	"database/sql"
+	"os"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db *database.Queries
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(dbConn)
+
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
 	}
 
 	mux := http.NewServeMux()
